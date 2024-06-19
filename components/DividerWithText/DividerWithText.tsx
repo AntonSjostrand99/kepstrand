cab717ccce273a060ed82f684d2cc7d46e4d426f
Anton Sjöstrand <asjoa99@hotmail.com>
2024-06-19 19:08:42 +0200

    fix the deploy error

--- components/DividerWithText/DividerWithText.tsx
+++ components/DividerWithText/DividerWithText.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const DividerWithText = ({ text }) => {
+// Definiera en interface för props
+interface DividerWithTextProps {
+  text: string;
+}
+
+const DividerWithText: React.FC<DividerWithTextProps> = ({ text }) => {
   return (
-    <div className="relative flex items-center justify-center my-10 bg-white border-4 border-black ">
+    <div className="relative flex items-center justify-center my-10 bg-white border-4 border-black">
       <hr className="w-full border-4 border-custom-yellow" />
       <span className="absolute sm:text-2xl md:text-3xl text-xl font-bold mb-4 flex justify-center text-black p-4 rounded-lg border-4 border-black shadow-md bg-custom-yellow">
         {text}
